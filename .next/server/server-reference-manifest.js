3b1f7775a2f87bf27691cfa77537c069a73513d9
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-04-10 11:39:47 +0530

    fixes some bugs

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"s0sEmtR/UL5f+j0CQGvLRGM771rZ10YJPWWCyQ0gxSU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"NeWnKtrm6kBRaQByFRAOOWQ7Sv9Q0hFOVxFDFPJISro=\"}"
